7ffc7d94c126e160326d1d89570eef6839781fed
brooks <brooks.houck@gmail.com>
2022-07-22 20:05:53 +0000

    added security_group

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@ module "auto-scaling" {
   repo-name = var.repo-name
   #vpc_zone_identifier = module.networking.vpc.vpc_id
   subnet_id = module.networking.vpc.public_subnets[0]
+  security_group = [module.networking.webserver-sg.id]
 
 }
 
