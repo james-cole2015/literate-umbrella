aa05c8871b17d82e648a89c95fc6b715f787d03d
brooks <brooks.houck@gmail.com>
2022-07-23 15:11:39 +0000

    updating webserver sg name to include repo-name

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -15,7 +15,7 @@ module "vpc" {
 
 ### CHANGE SSH BACK WHEN YOU CREATE THE BASTION HOST!! 
 resource "aws_security_group" "webserver-sg" {
-  name        = "webserver-sg"
+  name        = "webserver-sg-${var.repo-name}"
   description = "Allow webserver traffic"
   vpc_id      = module.vpc.vpc_id
 
@@ -70,4 +70,59 @@ resource "aws_security_group" "webserver-sg" {
 
 data "http" "terraform_ip" {
   url = "http://ipv4.icanhazip.com"
+}
+
+
+resource "aws_security_group" "bastion-host-sg" {
+  name        = "bastion-host-sg-${var.repo-name}"
+  description = "Allow bastion host traffic"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    description = "SSH from the hosting server"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${chomp(data.http.terraform_ip.body)}/32"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  /*
+  ingress {
+    description = "http from the internet"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  */
+  tags = {
+    Name = "${var.repo-name}-SG"
+  }
+  ingress {
+    description = "https from the internet"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
